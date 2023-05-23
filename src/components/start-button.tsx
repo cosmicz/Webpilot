f40b6d05d320560e9c07b20a5303ca3700c6649c
Adrian Spanu <adri@nspanu.ca>
2023-05-23 14:29:24 -0700

    Adding the test chain execution

--- src/components/start-button.tsx
+++ src/components/start-button.tsx
@@ -1,4 +1,5 @@
 import { MESSAGING_EVENT } from '@/config'
+import useConfig from '@/hooks/use-config'
 import { processHtml } from '@/magellan/parsers'
 import { sendToContentScript } from '@plasmohq/messaging'
 
@@ -10,10 +11,17 @@ const startAnalysis = async () => {
   // Pull the current state (i.e. document.documentElement.outerHTML)
   // Send the current state to tickle agent, kick off that whole process
 
+  const { config, setConfig } = useConfig()
+
   const docString = await sendToContentScript({ name: MESSAGING_EVENT.GET_DOCUMENT })
   console.log("The doc string I got: " + docString)
 
   const chunks = processHtml(docString)
 
   console.log(chunks)
+
+  const testChain = executeTestChain(config.authKey, docString)
+
+  console.log("Test chain: " + testChain)
+  return testChain
 }
